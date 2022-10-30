6767310b65acfb77b77199718f8dbdf843f2a814
knotmyrealname <danieldong1.618@gmail.com>
2022-10-30 03:39:43 -0400

    Improving demo script and adding json support

--- demos/merged_demo/run_demo.sh
+++ demos/merged_demo/run_demo.sh
@@ -1,41 +1,67 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
 echo "Generating Demo Files..."
 
-echo "Text file (t), Standard Output (s), or Clean (c):" 
+echo "Text file (t), Standard Output (s), JSON (j), or Clean (c):" 
 read output_format
 
+function demorosie (){
+  rosie -f demo.rpl grep -w -o $1 $2 $input_file
+}
 
 if [[ $output_format == "t" || $output_format == "T" ]]
 then
+  echo "Input file:"
+  read input_file
   echo "Running Demo 1... (A pattern that matches lines)"
-  rosie --colors "tx_begin_line=red:end_line=blue:data_line=green" -f demo.rpl grep -w -o color line input-1.txt >> output_1.txt
+  demorosie color line >> output_1.txt
   echo "Running Demo 2... (A pattern that matches simple transactions)"
-  rosie --colors "tx_begin_line=red:tx_end_line=blue:data_line=green" -f demo.rpl grep -w -o color simple_tx input-1.txt >> output_2.txt
+  demorosie color simple_tx >> output_2.txt
   echo "Running Demo 3... (A pattern that matches nested transactions)"
-  rosie --colors "tx_begin_line=red:tx_end_line=blue:data_line=green" -f demo.rpl grep -w -o color nested_tx input-1.txt >> output_3.txt
+  demorosie color nested_tx >> output_3.txt
   echo "Running Demo 4... (A pattern that matches nested, incomplete, and invalid transactions)"
-  rosie --colors "tx_begin_line=red:tx_end_line=blue:data_line=green:invalid_line=yellow:incomplete_tx_begin_line=cyan" -f demo.rpl grep -w -o color nested_all_tx input-1.txt >> output_4.txt
+  demorosie color nested_all_tx >> output_4.txt
 
 elif [[ $output_format == "s" || $output_format == "S" ]]
 then
+  echo "Input file:"
+  read input_file
   echo "Running Demo 1... (A pattern that matches lines)"
-  rosie --colors "tx_begin_line=red:end_line=blue:data_line=green" -f demo.rpl grep -w -o color line input-1.txt
+  demorosie color line
   echo "Hit {Enter} to continue"
   read entry
   echo "Running Demo 2... (A pattern that matches simple transactions)"
-  rosie --colors "tx_begin_line=red:tx_end_line=blue:data_line=green" -f demo.rpl grep -w -o color simple_tx input-1.txt
+  demorosie color simple_tx
   echo "Hit {Enter} to continue"
   read entry
   echo "Running Demo 3... (A pattern that matches nested transactions)"
-  rosie --colors "tx_begin_line=red:tx_end_line=blue:data_line=green" -f demo.rpl grep -w -o color nested_tx input-1.txt
+  demorosie color nested_tx
   echo "Hit {Enter} to continue"
   read entry
   echo "Running Demo 4... (A pattern that matches nested, incomplete, and invalid transactions)"
-  rosie --colors "tx_begin_line=red:tx_end_line=blue:data_line=green:invalid_line=yellow:incomplete_tx_begin_line=cyan" -f demo.rpl grep -w -o color nested_all_tx input-1.txt
+  demorosie color nested_all_tx
+
+elif [[ $output_format == "j" || $output_format == "J" ]]
+then
+  echo "Input file:"
+  read input_file
+  echo "Running Demo 1... (A pattern that matches lines)"
+  demorosie jsonpp line >> json_1.txt
+  echo "Running Demo 2... (A pattern that matches simple transactions)"
+  demorosie jsonpp simple_tx >> json_2.txt
+  echo "Running Demo 3... (A pattern that matches nested transactions)"
+  demorosie jsonpp nested_tx >> json_3.txt
+  echo "Running Demo 4... (A pattern that matches nested, incomplete, and invalid transactions)"
+  demorosie jsonpp nested_all_tx >> json_4.txt
 
 elif [[ $output_format == "c" || $output_format == "C" ]]
 then
   echo "Cleaning up output"
-  declare -a files=('output_1.txt' 'output_2.txt' 'output_3.txt' 'output_4.txt') 
+  declare -a files=('output_1.txt' 'output_2.txt' 'output_3.txt' 'output_4.txt'
+                    'json_1.txt' 'json_2.txt' 'json_3.txt' 'json_4.txt') 
   for f in "${files[@]}";
   do
     if [ -f "$f" ]
@@ -45,7 +71,7 @@ then
   done
 
 else
-echo "Wrong input"
+echo "Wrong input" >&2
 fi
 
 echo "Done"
